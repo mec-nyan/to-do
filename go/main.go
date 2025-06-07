18b15c978f105b7de14abb18ae8f04a3fb3e5d46
mec-nyan <86024238+mec-nyan@users.noreply.github.com>
2025-06-06 23:58:43 -0300

    Add persistence.

--- go/main.go
+++ go/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"os"
 	"strings"
@@ -14,7 +15,36 @@ type mode int
 const (
 	modeList mode = iota
 	modeInput
+
+	dataFile = "todos.json"
 )
+
+func loadTodos() []string {
+	data, err := os.ReadFile(dataFile)
+	if err != nil {
+		return []string{}
+	}
+
+	var todos []string
+	if err := json.Unmarshal(data, &todos); err != nil {
+		fmt.Println("Error loading todos:", err)
+		return []string{}
+	}
+
+	return todos
+}
+
+func saveTodos(todos []string) {
+	data, err := json.MarshalIndent(todos, "", "  ")
+	if err != nil {
+		fmt.Println("Error saving todos:", err)
+		return
+	}
+	err = os.WriteFile(dataFile, data, 0644)
+	if err != nil {
+		fmt.Println("Error writing file:", err)
+	}
+}
 
 type model struct {
 	todos     []string
@@ -33,7 +63,7 @@ func initialModel() model {
 	ti.Width = 80
 
 	return model{
-		todos:     []string{},
+		todos:     loadTodos(),
 		textInput: ti,
 		mode:      modeList,
 	}
@@ -67,6 +97,7 @@ func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 			case tea.KeyEnter:
 				if val := m.textInput.Value(); val != "" {
 					m.todos = append(m.todos, val)
+					saveTodos(m.todos)
 				}
 				m.mode = modeList
 				return m, nil
