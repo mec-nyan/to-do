e768cfae5965a441b6da15c5898215188482bddd
mec-nyan <86024238+mec-nyan@users.noreply.github.com>
2025-09-17 20:10:55 -0300

    Add example file and load items.

--- todo2/todo2.py
+++ todo2/todo2.py
@@ -8,9 +8,12 @@ A todo app for the terminal.
 
 import curses
 import sys
-import os
+import json
 
 # Check if there are previously saved items. If so, load them.
+# TODO: Check if the file exists.
+with open("items.json", "r") as saved:
+    items: dict[str, list[str]] = json.load(saved)
 
 
 # ┌───────────┐
@@ -35,6 +38,15 @@ def main(_):
     columns, rows = screen.getmaxyx()
 
     screen.addstr(f"Rows: {rows}\nCols: {columns}")
+    screen.getch()
+
+    screen.clear()
+
+    for k, v in items.items():
+        screen.addstr(f"{k}\n")
+        for bullet in v:
+            screen.addstr(f"\t{bullet}\n")
+
     screen.getch()
 
 
