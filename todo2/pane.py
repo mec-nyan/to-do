dc1454557ab63afcc94e1da4af25e5e294219928
mec-nyan <86024238+mec-nyan@users.noreply.github.com>
2025-09-18 20:03:22 -0300

    Minor improvements.

--- todo2/pane.py
+++ todo2/pane.py
@@ -17,6 +17,9 @@ import curses
 class Position:
     """
     A point on the screen. Usually the top left corner.
+
+
+    Default is 0, 0.
     """
 
     def __init__(self, x: int = 0, y: int = 0) -> None:
@@ -27,6 +30,8 @@ class Position:
 class Size:
     """
     The size of a rect (rows and columns).
+
+    Default is 0, 0 (will use max rows and cols (see curses.newwin)).
     """
 
     def __init__(self, width: int = 0, height: int = 0) -> None:
@@ -37,6 +42,8 @@ class Size:
 class Geometry:
     """
     Position and size.
+
+    Default is 0, 0, 0, 0 (see curses.newwin).
     """
 
     def __init__(self, pos: Position = Position(),
@@ -48,14 +55,28 @@ class Geometry:
 class Padding:
     """
     The padding of a Pane.
+
+    TODO: Maybe use default values (i.e 1, 2).
     """
 
-    def __init__(self, top: int = 0, right: int = 0, bottom: int = 0,
-                 left: int = 0) -> None:
-        self.top = top
-        self.right = right
-        self.bottom = bottom
-        self.left = left
+    def __init__(self, top: int = None, right: int = None, bottom: int = None,
+                 left: int = None) -> None:
+        self.top = 0
+        self.right = 0
+        self.bottom = 0
+        self.left = 0
+
+        # CSS style initialisation.
+        if top is not None:
+            self.top = top
+            self.bottom = top
+        if right is not None:
+            self.right = right
+            self.left = right
+        if bottom is not None:
+            self.bottom = bottom
+        if left is not None:
+            self.left = left
 
 
 class Margin:
@@ -63,7 +84,33 @@ class Margin:
     Maybe useful, maybe not.
     """
 
-    ...
+    def __init__(self, top: int = None, right: int = None, bottom: int = None,
+                 left: int = None) -> None:
+        self.top = 0
+        self.right = 0
+        self.bottom = 0
+        self.left = 0
+
+        # CSS style initialisation.
+        if top is not None:
+            self.top = top
+            self.bottom = top
+        if right is not None:
+            self.right = right
+            self.left = right
+        if bottom is not None:
+            self.bottom = bottom
+        if left is not None:
+            self.left = left
+
+
+class SimplePane:
+    """
+    SimplePlane pane has no borders nor padding.
+    """
+
+    def __init__(self, geometry: Geometry = Geometry()) -> None:
+        ...
 
 
 class Pane:
